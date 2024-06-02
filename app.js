dad14bffe2c67a4cdee7d4428edb6e07ffcc2826
Hanss121 <kangjihan121@gmail.com>
2024-06-02 13:02:11 +0700

    baru1

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ import { sequelize } from "./models/index.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
-import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const __dirname = path.resolve();
 
 // Middleware untuk parsing body JSON
 app.use(bodyParser.json());
-app.use(cors());
 
 // Middleware untuk melayani file statis
 app.use("/public", express.static(path.join(__dirname, "public")));
